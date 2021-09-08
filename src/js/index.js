59bea1f0800a77ec7fe6ce11e8bb8376256ff5d4
ysthekey <ysthekey@gmail.com>
2021-09-09 04:34:42 +0900

    fix bundle.js injection position in HTML.

--- src/js/index.js
+++ src/js/index.js
@@ -86,78 +86,83 @@ $(function () {
 });
 
 // フォーム
-const myForm = document.getElementById('contact-form');
-myForm.addEventListener('submit', handleSubmit);
+window.addEventListener('DOMContentLoaded', (event) => {
+  const myForm = document.getElementById('contact-form');
+  myForm.addEventListener('submit', handleSubmit);
 
-function handleSubmit(e) {
-  e.preventDefault();
-  let type = myForm.elements[0];
-  let name = myForm.elements[2];
-  let email = myForm.elements[3];
-  let contents = myForm.elements[5];
-  if (validator.equals(type.value, '未選択')) {
-    type.parentNode.nextElementSibling.textContent =
-      'ご用件の選択をお願いいたします。';
-    type.parentNode.nextElementSibling.style.color = 'red';
-    type.style.borderColor = 'red';
-  } else {
-    type.parentNode.nextElementSibling.textContent = '';
-    type.style.borderColor = '#404040';
-  }
-  if (validator.isEmpty(name.value)) {
-    name.nextElementSibling.textContent = 'お名前の欄は必須となっております。';
-    name.nextElementSibling.style.color = 'red';
-    name.style.borderColor = 'red';
-  } else {
-    name.nextElementSibling.textContent = '例：鈴木 太郎';
-    name.nextElementSibling.style.color = '#404040';
-    name.style.borderColor = '#404040';
-  }
-  if (validator.isEmpty(email.value)) {
-    email.nextElementSibling.textContent =
-      'メールアドレスの欄は必須となっております。';
-    email.nextElementSibling.style.color = 'red';
-    email.style.borderColor = 'red';
-  } else if (!validator.isEmail(email.value)) {
-    email.nextElementSibling.textContent =
-      '正しいメールアドレスをご入力お願いいたします。';
-    email.nextElementSibling.style.color = 'red';
-    email.style.borderColor = 'red';
-  } else {
-    email.nextElementSibling.textContent = '例：example@email.jp';
-    email.nextElementSibling.style.color = '#404040';
-    email.style.borderColor = '#404040';
-  }
-  if (validator.isEmpty(contents.value)) {
-    contents.nextElementSibling.textContent =
-      'お問い合わせ内容のご入力をお願いいたします。';
-    contents.nextElementSibling.style.color = 'red';
-    contents.style.borderColor = 'red';
-  } else {
-    contents.nextElementSibling.textContent = '';
-    contents.style.borderColor = '#404040';
-  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    let type = myForm.elements[0];
+    let name = myForm.elements[2];
+    let email = myForm.elements[3];
+    let contents = myForm.elements[5];
+    if (validator.equals(type.value, '未選択')) {
+      type.parentNode.nextElementSibling.textContent =
+        'ご用件の選択をお願いいたします。';
+      type.parentNode.nextElementSibling.style.color = 'red';
+      type.style.borderColor = 'red';
+    } else {
+      type.parentNode.nextElementSibling.textContent = '';
+      type.style.borderColor = '#404040';
+    }
+    if (validator.isEmpty(name.value)) {
+      name.nextElementSibling.textContent =
+        'お名前の欄は必須となっております。';
+      name.nextElementSibling.style.color = 'red';
+      name.style.borderColor = 'red';
+    } else {
+      name.nextElementSibling.textContent = '例：鈴木 太郎';
+      name.nextElementSibling.style.color = '#404040';
+      name.style.borderColor = '#404040';
+    }
+    if (validator.isEmpty(email.value)) {
+      email.nextElementSibling.textContent =
+        'メールアドレスの欄は必須となっております。';
+      email.nextElementSibling.style.color = 'red';
+      email.style.borderColor = 'red';
+    } else if (!validator.isEmail(email.value)) {
+      email.nextElementSibling.textContent =
+        '正しいメールアドレスをご入力お願いいたします。';
+      email.nextElementSibling.style.color = 'red';
+      email.style.borderColor = 'red';
+    } else {
+      email.nextElementSibling.textContent = '例：example@email.jp';
+      email.nextElementSibling.style.color = '#404040';
+      email.style.borderColor = '#404040';
+    }
+    if (validator.isEmpty(contents.value)) {
+      contents.nextElementSibling.textContent =
+        'お問い合わせ内容のご入力をお願いいたします。';
+      contents.nextElementSibling.style.color = 'red';
+      contents.style.borderColor = 'red';
+    } else {
+      contents.nextElementSibling.textContent = '';
+      contents.style.borderColor = '#404040';
+    }
 
-  if (
-    !validator.equals(type.value, '未選択') &&
-    !validator.isEmpty(name.value) &&
-    !validator.isEmpty(email.value) &&
-    validator.isEmail(email.value) &&
-    !validator.isEmpty(contents.value)
-  ) {
-    let formData = new FormData(myForm);
-    console.log('success');
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: new URLSearchParams(formData).toString(),
-    })
-      .then(() => console.log('Form successfully submitted'))
-      .catch((error) => alert(error));
-  } else {
-    $('html,body').animate({ scrollTop: $('#contact-form').offset().top - 80 });
+    if (
+      !validator.equals(type.value, '未選択') &&
+      !validator.isEmpty(name.value) &&
+      !validator.isEmpty(email.value) &&
+      validator.isEmail(email.value) &&
+      !validator.isEmpty(contents.value)
+    ) {
+      let formData = new FormData(myForm);
+      console.log('success');
+      fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: new URLSearchParams(formData).toString(),
+      })
+        .then(() => console.log('Form successfully submitted'))
+        .catch((error) => alert(error));
+    } else {
+      $('html,body').animate({
+        scrollTop: $('#contact-form').offset().top - 80,
+      });
+    }
   }
-}
+});
 
 // パーティクル(MV)
 window.particlesJS(
